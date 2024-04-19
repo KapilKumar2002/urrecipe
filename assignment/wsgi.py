ed1933d4b062e2dc1a90fbca135c3687442f4b3e
KapilKumar2002 <kapil012002@gmail.com>
2024-04-19 21:54:59 +0530

    some fixed

--- assignment/wsgi.py
+++ assignment/wsgi.py
@@ -16,3 +16,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assignment.settings")
 application = Cling(get_wsgi_application())
 application = get_wsgi_application()
+
+app = application
